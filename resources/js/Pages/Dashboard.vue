3f96df807ff9cfb7d5e614518be1526da3061093
m4Liiii <malekhiw04@gmail.com>
2024-06-24 20:36:46 +0300

    vbf

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,7 +14,7 @@ import { Link } from "@inertiajs/vue3";
     <div class="py-12">
       <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="overflow-hidden shadow-sm bg-stone-300 sm:rounded-lg">
-          <div class="p-6 text-cyan-800">بەخێر mبێیت</div>
+          <div class="p-6 text-cyan-800">بەخێر mxبێیت</div>
         </div>
       </div>
     </div>
